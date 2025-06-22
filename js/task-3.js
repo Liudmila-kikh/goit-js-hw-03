2edf0ded8dbd4a1349f6c98eee6d7dee571710f6
LIUDMILA KIK <tender.ird@gmail.com>
2025-06-22 23:58:02 +0200

    js3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,17 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+function filterArray(numbers, value) {
+    const filteredNumbers = [];
 
-    return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    for (let i = 0; i < numbers.length; i++) {
+
+        if (numbers[i] > value) {
+            filteredNumbers.push(numbers[i]);
+        }
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(filterArray([1, 2, 3, 4, 5], 3));
+console.log(filterArray([1, 2, 3, 4, 5], 4));
+console.log(filterArray([1, 2, 3, 4, 5], 5));
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
